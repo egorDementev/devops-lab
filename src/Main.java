4a0268c39e12434e6cab3a71ee36074f7407be43
Egor <dementev.eg@yandex.ru>
2023-06-01 16:07:02 +0300

    main files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        Scanner console = new Scanner(System.in);
+        System.out.println("""
+                                        
+                    Hello!!
+                    Please enter:
+                    complex - if you want to work with complex numbers
+                    matrix - if you want to work with matrix
+                                        
+                    """);
+        String choice = console.nextLine();
+        if (Objects.equals(choice, "complex")) {
+            ComplexIO start = new ComplexIO();
+            start.WorkWithComplex();
+        }
+        else if (Objects.equals(choice, "matrix")) {
+            MatrixIO start = new MatrixIO();
+            start.WorkWithMatrix();
+        }
     }
 }
